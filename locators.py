711ef25ced746c07f98a78ce702b3568f31b5282
n00bcybot <freshmaker21@gmail.com>
2022-02-01 14:39:53 +0100

    Setting up locators

---
+++ locators.py
@@ -0,0 +1,14 @@
+import maya.cmds as cmds
+
+# select vertices and spawn locator at each vertex' position
+
+vertex_list = cmds.ls(sl=True, fl=True)                    #create list from selected vertices
+for i in vertex_list:
+    print(i)
+
+vertex_position = []                                       # create empty list to add to
+
+for i in vertex_list:                                      # get position of all selected vertices
+    vertex_position.append(cmds.pointPosition(i, l=True))
+for i in vertex_position:
+    position = cmds.spaceLocator(position=i)               # spawn locator at the positions in the list

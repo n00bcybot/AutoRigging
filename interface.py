1e217124e39231d171ca50b44f29c2cf98d32abc
n00bcybot <freshmaker21@gmail.com>
2022-02-02 17:28:09 +0100

    Created simple interface with class approach. Not sure yet if class is actually necessary, since I have gotten good results with just a function before

--- interface.py
+++ interface.py
@@ -1,3 +1,27 @@
 import maya.cmds as cmds
 
-cmds.polyCube()
+
+class MyWindow:
+    def __init__(self):
+        self.window = 'My Window'
+        self.title = 'Rigging Tools'
+        self.size = (400, 400)
+        self.button = 'button_makeCube'
+
+        def makeCube():
+            cmds.polyCube()
+
+        if cmds.window(self.window, exists=True):
+            cmds.deleteUI(self.window, window=True)
+
+        self.window = cmds.window(self.window, title=self.title, widthHeight=self.size)
+        cmds.columnLayout(adjustableColumn=True)
+
+        cmds.separator(height=20)
+        cmds.button(self.button, label='Make Cube', command=makeCube)
+        cmds.separator(height=20)
+
+        cmds.showWindow()
+
+
+windowMain = MyWindow()

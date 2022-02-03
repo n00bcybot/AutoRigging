1aaa33ae6f4d7f10f9651605d1fc26f41f8e934f
n00bcybot <freshmaker21@gmail.com>
2022-02-03 22:19:51 +0100

    Created function which replaces a string with another for each item in string list

--- locators.py
+++ locators.py
@@ -8,8 +8,8 @@ def vertexToLocator():
     vertexList = cmds.ls(selection=True, flatten=True)                     # create list from selected vertices
     vertexPosition = []                                                    # create empty list to add to
     for i in vertexList:                                                   # get position of all selected vertices
-        vertexPosition.append(cmds.pointPosition(i, local=True))
-    for i in vertexPosition:
+        vertexPosition.append(cmds.pointPosition(i, local=True))           #
+    for i in vertexPosition:                                               #
         cmds.spaceLocator(position=i)                                      # spawn locator at the positions in the list
 
 
@@ -17,8 +17,8 @@ def vertexToLocator():
 
 jointList = cmds.ls(et="joint")                                            # create list from selected joints
 removeString = "r_"                                                        # define string to remove
-
-
+                                                                           #
+                                                                           #
 def removePrefix(list, prefix):                                            # recursive function
     for i in list:                                                         #
         if prefix in i:                                                    # as long as this condition is true, the

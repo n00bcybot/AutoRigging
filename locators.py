41427d106de8572c11155ee4c7a4127aef15fca9
n00bcybot <freshmaker21@gmail.com>
2022-02-03 22:16:29 +0100

    Created function which replaces a string with another for each item in string list

--- locators.py
+++ locators.py
@@ -15,13 +15,31 @@ def vertexToLocator():
 
 ########################################################################################################################
 
-jointList = cmds.ls(et="joint")                                 # create list from selected joints
-removeString = "r_"                                             # define string to remove
+jointList = cmds.ls(et="joint")                                            # create list from selected joints
+removeString = "r_"                                                        # define string to remove
 
-def removePrefix(list, prefix):                                 # recursive function
-    for i in list:                                              #
-        if prefix in i:                                         # as long as this condition is true, the
-            list.remove(i)                                      #
-            removePrefix(list, prefix)                          # function will keep calling itself
+
+def removePrefix(list, prefix):                                            # recursive function
+    for i in list:                                                         #
+        if prefix in i:                                                    # as long as this condition is true, the
+            list.remove(i)                                                 #
+            removePrefix(list, prefix)                                     # function will keep calling itself
+
+########################################################################################################################
+
+def replaceString(inputlist, string, newString, outputList):               # replace a string with another for each
+    for i in inputlist:                                                    # item in list empty output list must be
+        if string in i:                                                    # defined for this to work
+            temp = i.replace(string, newString)                            #
+            outputList.append(temp)                                        #
+    return outputList                                                      #
+
+list = ["r_apple_jnt", "r_pair_jnt", "r_banana_jnt", "r_orange_jnt", "strawberry", "raspberry", "blueberry"]
+
+z = []
+x = "_jnt"
+y = "_loc"
+
+print(replaceString(list, x, y, z))
 
 ########################################################################################################################
--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 import maya.cmds as cmds
 
+jointList = cmds.ls(et="joint")  # create list from selected joints
+removeString = "r_"  # define string to remove
+
+
+def removePrefix(list, prefix):  # recursive function
+    for i in list:  #
+        if prefix in i:  # as long as this condition is true, the
+            list.remove(i)  #
+            removePrefix(list, prefix)  # function will keep calling itself
+
+
+def replaceString(list, string, stringNew):
+    for i in list:
+        if string in i:
+            i.replace(string, stringNew)
+
+
+removePrefix(jointList, removeString)
+x = "_jnt"
+y = "_loc"
+replaceString(jointList, x, y)

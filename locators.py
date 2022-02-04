96f5a276373c6b4a3da5c7da00f9312c315987a0
n00bcybot <freshmaker21@gmail.com>
2022-02-04 19:04:05 +0100

    This block creates a list of locators and spawns joints in their place

--- locators.py
+++ locators.py
@@ -43,3 +43,20 @@ y = "_loc"
 print(replaceString(list, x, y, z))
 
 ########################################################################################################################
+# Creates joint chain from locators list
+
+def spawnJoints(list):                                                  #
+    cmds.select(deselect=True)                                          # In case there is anything selected in the
+    locatorsPosition = []                                               # scene, this command deselects it - otherwise
+    for i in list:                                                      # messes up the command
+        locatorsPosition.append(cmds.pointPosition(i, world=True))      #
+    for i, j in zip(locatorsPosition, list):                            #
+        cmds.joint(position=i, name=j+"_jnt")                           # This line renames the joint
+    for i in list:                                                      # This loop deletes all locators use for
+        cmds.delete(i)                                                  # the joint creation
+                                                                        #
+locatorsList = ['locator45', 'locator46', 'locator47']                  # Sample list
+
+spawnJoints(locatorsList)
+
+########################################################################################################################
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import maya.cmds as cmds
 
-locatorsList = cmds.ls(sl=True)
 
+def spawnJoints(list):
+    cmds.select(deselect=True)
+    locatorsPosition = []
+    for i in list:
+        locatorsPosition.append(cmds.pointPosition(i, world=True))
+    for i, j in zip(locatorsPosition, list):
+        cmds.joint(position=i, n=j+string)
+    for i in list:
+        cmds.delete(i)
 
+locatorsList = ['locator45', 'locator46', 'locator47']
+string = "_jnt"
 
-cmds.select(deselect=True)
-locatorsPosition = []
-for i in locatorsList:
-    locatorsPosition.append(cmds.pointPosition(i, world = True))
-
-for i in locatorsPosition:
-    cmds.joint(position = i)
-
-
+spawnJoints(locatorsList)
